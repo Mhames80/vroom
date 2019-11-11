5a9aeb9d22d45eba33bb4ef01437536f52d6da8d
Yola <200410745@student.georgianc.on.ca>
2019-11-11 13:31:59 -0500

    I made some models that only have Id. Nothing that cant be changed. I just looked at the video game store to see what might be a model.

---
+++ group1/Models/Shop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace group1.Models
+{
+    public class Shop
+    {
+        public virtual int Id { get; set; }
+    }
+}
