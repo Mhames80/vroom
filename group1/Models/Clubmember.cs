e8d4499bc8da38ce6705072208367b5c34750d74
Mark <200395429@student.georgianc.on.ca>
2019-11-13 17:45:23 -0500

    Club member model

---
+++ group1/Models/Clubmember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace group1.Models
+{
+    public class Clubmember
+    {
+        public virtual int Id { get; set; }
+    }
+}
